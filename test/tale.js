fec62127d2524eb844a17255fcb3de649991daef
PolezElena <lena.polezhaewa2012@yandex.ru>
2024-01-30 13:51:36 +0300

    dds

--- test/tale.js
+++ test/tale.js
@@ -3,8 +3,8 @@ import { kolobok, newYear } from "./module.js"
 console.log(kolobok('Дедушка'))
 console.log(kolobok('Заяц'))
 console.log(kolobok('Лиса'))
-//console.log(kolobok('Волк'))
+console.log(kolobok('Волк'))
 
 console.log(newYear('Дед Мороз'))
 console.log(newYear('Снегурка'))
-//console.log(newYear('Бабайка'))
+console.log(newYear('Бабайка'))
